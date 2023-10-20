75695c7018da9ae9e2921ab1034b06d429845a6c
Ryan Soeyadi <rsoeyadi@Ryans-MBP.verizon.net>
2023-10-19 21:55:01 -0400

    removed some code

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-import uvicorn
 
 app = FastAPI()
 
@@ -7,6 +6,3 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "You owe $10 for that book you never returned."}
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
